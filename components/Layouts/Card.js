90c086b3611c618cd8c43a284a04e972834ee92d
Chiraphon Worasuk <55229406+chiraphon-w@users.noreply.github.com>
2021-04-19 18:04:27 +0700

    Update Card.js

--- components/Layouts/Card.js
+++ components/Layouts/Card.js
@@ -1,15 +1,6 @@
 import React from 'react';
 
 export default function Card({ title, children }) {
-
-  let d = new Date();
-  let ye = new Intl.DateTimeFormat('en', { year: '2-digit' }).format(d);
-  let mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d);
-  let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
-  let hms = new Intl.DateTimeFormat('en', { hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(d);
-
-  let realTime = (`Added on ${mo} ${da}, ${ye}, ${hms}`);
-
   return (
       
       <div className="md:inner md:w-1/2 pb-4 md:pr-4">
