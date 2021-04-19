767c641fa2879076d43b6448c5c27c10bd1e2d5c
Chiraphon Worasuk <55229406+chiraphon-w@users.noreply.github.com>
2021-04-19 17:55:54 +0700

    edit Buttons

--- components/WidgetTools.js
+++ components/WidgetTools.js
@@ -3,65 +3,64 @@ import Modal from "../components/Modal";
 import { RiAddCircleLine, RiIncreaseDecreaseLine } from "react-icons/ri";
 import { BiBomb } from "react-icons/bi";
 
-import WidgetsMenu from "./WidgetsMenu";
 import WidgetsCard from "./Layouts/WidgetsCard";
 import { AiOutlineMessage } from "react-icons/ai";
 import { IoTimerOutline } from "react-icons/io5";
 
-import AddWidgCard from "./Layouts/AddWidgCard";
-import Button from "./Buttons/Button";
 import Card from "./Layouts/Card";
 import AddJustSay from "./AddWidgets/AddJustSay";
 import AddCounter from "./AddWidgets/AddCounter";
 import JustSay from "./JustSay";
+import Button from "./Buttons/Button";
 
 export default function WidgetTools() {
-  const [modalActive, setModalActive] = useState(false);
-  const [modalActive1, setModalActive1] = useState(false);
-  const [modalActive2, setModalActive2] = useState(false);
-  const [modalActive3, setModalActive3] = useState(false);
+  const [modalActiveMenu, setModalActiveMenu] = useState(false);
+  const [modalActiveJustSay, setModalActiveJustSay] = useState(false);
+  const [modalActiveCounter, setModalActiveCounter] = useState(false);
 
-  const [title1, setTitle1] = useState("");
+  const [justSay, setJustSay] = useState("");
   const [listAllWidgets, setListAllWidgets] = useState([]);
 
-
   const handleClick = function () {
-    setModalActive(true);
+    setModalActiveMenu(true);
   };
-  const handleCancel = function () {
-    setModalActive(false);
-  };
-
   const handleClick1 = function () {
-    setModalActive1(true);
-    setModalActive(false);
-    setTitle1();
-    // setTest1()
+    setModalActiveJustSay(true);
+    setModalActiveMenu(false);
+    setJustSay();
   };
   const handleClick2 = function () {
-    setModalActive2(true);
-    setModalActive(false);
+    setModalActiveCounter(true);
+    setModalActiveMenu(false);
   };
   const handleClick3 = function () {
-    setModalActive3(true);
-    setModalActive(false);
+    setModalActiveMenu(false);
   };
-  const handleCancels = function () {
-    setModalActive(false);
-    setModalActive1(false);
-    setModalActive2(false);
-    setModalActive3(false);
+  const handleCancel = function () {
+    setModalActiveMenu(false);
+    setModalActiveJustSay(false);
+    setModalActiveCounter(false);
   };
-  // const test = function () {
-  //     alert(test1);
-  // };
+
+  let d = new Date();
+  let ye = new Intl.DateTimeFormat("en", { year: "2-digit" }).format(d);
+  let mo = new Intl.DateTimeFormat("en", { month: "short" }).format(d);
+  let da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
+  let hms = new Intl.DateTimeFormat("en", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(d);
+
+  const realTime = `Added on ${mo} ${da}, ${ye}, ${hms}`;
+
   let ct = "mx-auto text-4xl";
-  //   console.log(title1, "xy");
+  let disabled = true;
 
   const handleJustsay = function () {
-    if (listAllWidgets.length > 0 ) {
+    if (listAllWidgets.length > 0) {
       console.log(listAllWidgets);
-      return <JustSay title={title1} listAllWidgets={listAllWidgets} />;
+      return <JustSay title={justSay} listAllWidgets={listAllWidgets} />;
     } else {
       return (
         <Card title=" ">
@@ -86,22 +85,28 @@ export default function WidgetTools() {
 
   return (
     <div className="mb-4 space-x-1">
-      <button
+      <Button doClick={handleClick} disabled={!disabled}>
+        <RiAddCircleLine className="inline-block text-xl relative -top-0.5" /> Add Widget
+      </Button>
+      {/* <button
         onClick={handleClick}
         className="text-white focus:outline-none px-4 py-1 rounded-md bg-blue-500 hover:bg-blue-600"
       >
         <RiAddCircleLine className="inline-block text-xl relative -top-0.5" />{" "}
         Add Widget
-      </button>
-      <button className="text-white focus:outline-none px-4 py-1 rounded-md bg-gray-300">
+      </button> */}
+
+      <Button disabled={disabled}>
         <BiBomb className="inline-block text-xl relative -top-0.5" /> Clear all
-      </button>
+      </Button>
+      {/* <button className="text-white focus:outline-none px-4 py-1 rounded-md bg-gray-300">
+        <BiBomb className="inline-block text-xl relative -top-0.5" /> Clear all
+      </button> */}
 
       <div className="pt-3.5">{handleJustsay()}</div>
 
-      {modalActive && (
+      {modalActiveMenu && (
         <Modal onCancel={handleCancel}>
-          {/* <WidgetsMenu /> */}
           <h2 className="text-xl undefined">Add widget</h2>
           <div className=" flex flex-wrap text-center mt-1.5 -ml-1.5">
             <WidgetsCard title="JustSay">
@@ -117,22 +122,20 @@ export default function WidgetTools() {
         </Modal>
       )}
 
-      {modalActive1 && (
-        <Modal onCancel={handleCancels}>
+      {modalActiveJustSay && (
+        <Modal onCancel={handleCancel}>
           <AddJustSay
-            setTitle1={setTitle1}
+            setJustSay={setJustSay}
             handleJustsay={handleJustsay}
-            handleCancels={handleCancels}
+            handleCancel={handleCancel}
             setListAllWidgets={setListAllWidgets}
             listAllWidgets={listAllWidgets}
+            realTime={realTime}
           />
         </Modal>
       )}
-      {modalActive2 && (
-        <Modal onCancel={handleCancels}>
-          {/* <AddWidgCard title="Add Counter" text="Enter the initial value ">
-                        <Button color='primary'>Add</Button>
-                    </AddWidgCard> */}
+      {modalActiveCounter && (
+        <Modal onCancel={handleCancel}>
           <AddCounter />
         </Modal>
       )}
