94e59fb4442be56f9065f2039481867077e8884f
Chiraphon Worasuk <55229406+chiraphon-w@users.noreply.github.com>
2021-04-19 18:06:37 +0700

    Update Widgets.js

--- components/Widgets.js
+++ components/Widgets.js
@@ -3,18 +3,9 @@ import JustSay from "./JustSay";
 import Timer from "./Timer";
 import Counter from "./Counter";
 import WidgetTools from "./WidgetTools";
-import NoWidgets from "./NoWidgets";
-import Modal from "./Modal";
-import AddJustSay from "../components/AddWidgets/AddJustSay";
-import WidgetsMenu from "../components/WidgetsMenu";
-import Test from "./NavBar/Test";
 
 export default function Widgets() {
-    const widgetsA = [
-    <JustSay key="13123123" />,
-    <Counter key="3235235" />,
-    <Timer key="0987876" />,
-  ];
+
   return (
     <>
       <h2 className="text-xl">Widgets</h2>
